6bf2a7c84520bf6b8aed94d5b04b5f53df712a0c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-27 19:24:14 +0900

    Initial commit

---
+++ src/test/java/com/example/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
