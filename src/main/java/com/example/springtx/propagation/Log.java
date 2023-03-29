134d9e3da1d4c0e227b48140d947d8661e19cd99
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-29 20:34:15 +0900

    feat: Log entity 추가

---
+++ src/main/java/com/example/springtx/propagation/Log.java
@@ -0,0 +1,27 @@
+package com.example.springtx.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Log {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String message;
+
+    public Log() {
+    }
+
+    public Log(String message) {
+        this.message = message;
+    }
+}
